ceb0dabd7760db49ebddfc05ab8cf0a2c9390c66
Eric D. Torres <etorresnotary@gmail.com>
2020-10-13 19:10:44 -0500

    updated with CRUD routes

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+//LOAD env vars
+dotenv.config({path: "./config/config.env"});
+
+const PORT = process.env.PORT || 27324;
+
 const app = express();
 
-//LOAD env vars
-dotenv.config({path: "./config/cong.env"});
+app.get("/api/v1/bootcamps", (req, res) => {
+    res.status(200).json({success: true, msg: "Show all bootcamps"});
+});
 
-const PORT = process.env.PORT || 27324
+app.get("/api/v1/bootamps/:id", (req, res) => {
+    res.status(200).json({success: true, msg: `Show bootcamp ${req.params.id}`});
+})
 
+app.post("/api/v1/bootamps", (req, res) => {
+    res.status(200).json({success: true, msg: "Create new bootcamp"});
+})
 
+app.put("/api/v1/bootamps/:id", (req, res) => {
+    res.status(200).json({success: true, msg: `Update bootcamp ${req.params.id}`});
+})
 
+app.delete("/api/v1/bootamps/:id", (req, res) => {
+    res.status(200).json({success: true, msg: `Delete bootcamp ${req.params.id}`});
+})
 
 app.listen(PORT, () => {
-console.log(`Server runing in ${process.env.NODE_ENV} monde on http://localhost:${PORT}`)
+console.log(`Server runing in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`)
 })
